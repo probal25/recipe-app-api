25aea3df69ae63a959a9f531f1232993ade0ab24
probal25 <probal25@hotmail.com>
2019-10-30 14:49:45 +0600

    Added custom Recipe Detail View

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -43,3 +43,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """Retrieve the recipes for the authenticted user"""
         return self.queryset.filter(user=self.request.user)
+    
+    def get_serializer_class(self):
+        """Return appropiat serializer class"""
+        if self.action == 'retrieve':
+            return serializers.RecipeDetailSerializer
+        
+        return self.serializer_class
